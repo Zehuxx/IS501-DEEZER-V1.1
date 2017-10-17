69233c0cf26bbb741d86d6a572d72b21968ab42f
gerardo1995 <gerardimar18@gmail.com>
2017-10-17 13:29:59 -0600

    mas cosas again

--- inicio.php
+++ inicio.php
@@ -17,11 +17,11 @@
     	  <div id="explorar"><a id="explorartxt">Explorar ></a></div>
     	  <div id="descubre"><p id="descubretxt">Descubre los canales de musica, entretenimiento y podcast</p></div>
     	  <div id="genero">
-    	  	<div id="pop" class="list"></div>
-    	  	<div id="momentos" class="list"></div>
-    	  	<div id="funk" class="list"></div>
-    	  	<div id="deporte" class="list"></div>
-    	  	<div id="niños" class="list"></div>
+    	  	<div id="metal" class="list"><span class="txt">Metal</span></div>
+    	  	<div id="Rock" class="list"><span class="txt">Rock</span></div>
+    	  	<div id="Dance" class="list"><span class="txt2">Dance / EDM</span></div>
+    	  	<div id="indie" class="list"><span class="txt">Indie</span></div>
+    	  	<div id="Momentos" class="list"><span class="txt1">Momentos y actividades</span></div>
     	  </div>
     	  <div id="seleccion"><a id="selecciontxt">Seleccion de playlist</a></div>
     	  <div id="selec"><p id="selectxt">Una seleccion de los editores para ti</p></div> 
@@ -42,7 +42,21 @@
     	  <div id="top_pais">
     	  	<div id="imgtop"></div>
     	  </div>
-    	  <div id="listacanciones"></div>
+    	  <div id="listacanciones">
+    	  	<h6>Canciones</h6>
+    	  	<div class="cancion"><span class="numc">01.</span><a class="nmbcancion">Iron Man</a><span class="artir">de </span><a class="arti">Black Sabbath</a></div>
+    	  	<div class="cancion"><span class="numc">02.</span><a class="nmbcancion">Land of confusion</a><span class="artir">de </span><a class="arti">Genesis</a></div>
+    	  	<div class="cancion"><span class="numc">03.</span><a class="nmbcancion">I want out</a><span class="artir">de </span><a class="arti">Helloween</a></div>
+    	  	<div class="cancion"><span class="numc">04.</span><a class="nmbcancion">Decadence</a><span class="artir">de </span><a class="arti">Disturbed</a></div>
+    	  	<div class="cancion"><span class="numc">05.</span><a class="nmbcancion">Evolution</a><span class="artir">de </span><a class="arti">Pearl Jam</a></div>
+    	  </div>
+    	  <div id="lanzamientos"><a id="lanzamientostxt">Nuevos Lanzamientos ></a></div>
+    	  <div id="lanzamiento">
+    	  	<div id="lanzamiento1" class="lanzamiet"></div>
+    	  	<div id="lanzamiento2" class="lanzamiet"></div>
+    	  	<div id="lanzamiento3" class="lanzamiet"></div>
+    	  	<div id="lanzamiento4" class="lanzamiet"></div>
+    	  </div>
         </center>
     </div>	
 
