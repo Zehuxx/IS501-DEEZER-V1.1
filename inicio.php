20f850a32ce5459e212d3ae1c93c11a467d7df4f
gerardo1995 <gerardimar18@gmail.com>
2017-11-13 21:45:40 -0600

    actualizacion

--- inicio.php
+++ inicio.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Inicio</title>
-	<meta charset="utf-8">
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="css/inicio.css">
-	<link rel="stylesheet" type="text/css" href="fantastic/css/font-awesome.min.css">
+    <title>Inicio</title>
+    <meta charset="utf-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/inicio.css">
+    <link rel="stylesheet" type="text/css" href="fantastic/css/font-awesome.min.css">
 </head>
 <body>
 <center>
@@ -50,37 +50,69 @@
   <center>
 
     <div id="contenedor">
-    	<center>
-    	  <!--<div id="flow">
+        <center>
+          <!--<div id="flow">
         
           </div>-->
-    	  <div id="explorar"><a id="explorartxt">Explorar ></a></div>
-    	  <div id="descubre"><p id="descubretxt">Descubre los canales de musica, entretenimiento y podcast</p></div>
-    	  <div id="genero">
-    	  	<div id="metal" class="list"><span class="txt">Metal</span></div>
-    	  	<div id="Rock" class="list"><span class="txt">Rock</span></div>
-    	  	<div id="Dance" class="list"><span class="txt2">Dance / EDM</span></div>
-    	  	<div id="indie" class="list"><span class="txt">Indie</span></div>
-    	  	<div id="Momentos" class="list"><span class="txt1">Momentos y actividades</span></div>
-    	  </div>
-    	  <div id="seleccion"><a id="selecciontxt">Seleccion de playlist</a></div>
-    	  <div id="selec"><p id="selectxt">Una seleccion de los editores para ti</p></div> 
-    	  <div id="playlist_editores">
-    	  	<div id="playlt1" class="playlt"></div>
-    	  	<div id="playlt2" class="playlt"></div>
-    	  	<div id="playlt3" class="playlt"></div>
-    	  	<div id="playlt4" class="playlt"></div>
-    	  </div>
-    	  <div id="popular"><a id="populartxt">Popular ></a></div>
-    	  <div id="popul">
-    	  	<div id="pop1" class="popl"></div>
-    	  	<div id="pop2" class="popl"></div>
-    	  	<div id="pop3" class="popl"></div>
-    	  	<div id="pop4" class="popl"></div>
-    	  </div>
-    	  <div id="exitos"><a id="exitostxt">Lista de exitos ></a></div>
-    	  <div id="top_pais">
-    	  	<div id="imgtop"></div>
+          <div id="explorar"><a id="explorartxt">Explorar ></a></div>
+          <div id="descubre"><p id="descubretxt">Descubre los canales de musica, entretenimiento y podcast</p></div>
+          <div id="genero">
+            <div id="metal" class="list"><span class="txt">Metal</span></div>
+            <div id="Rock" class="list"><span class="txt">Rock</span></div>
+            <div id="Dance" class="list"><span class="txt2">Dance / EDM</span></div>
+            <div id="indie" class="list"><span class="txt">Indie</span></div>
+            <div id="Momentos" class="list"><span class="txt1">Momentos y actividades</span></div>
+          </div>
+          <div id="seleccion"><a id="selecciontxt">Seleccion de playlist</a></div>
+          <div id="selec"><p id="selectxt">Una seleccion de los editores para ti</p></div> 
+          <div id="playlist_editores">
+            <div id="playlt1" class="playlt"></div>
+            <tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">116 canciones-48,777 fans</span></td>
+            </tr>
+            <div id="playlt2" class="playlt"></div>
+            <tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">116 canciones-48,777 fans</span></td>
+            </tr>
+            <div id="playlt3" class="playlt"></div>
+            <tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">116 canciones-48,777 fans</span></td>
+            </tr>
+            <div id="playlt4" class="playlt"></div>
+            <tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">116 canciones-48,777 fans</span></td>
+            </tr>
+          </div>
+          <div id="popular"><a id="populartxt">Popular ></a></div>
+          <div id="popul">
+            <div id="pop1" class="popl"></div>
+              <tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">116 canciones-48,777 fans</span></td>
+            </tr>
+            <div id="pop2" class="popl"></div>
+              <tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">116 canciones-48,777 fans</span></td>
+            </tr>
+            <div id="pop3" class="popl"></div>
+<tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">116 canciones-48,777 fans</span></td>
+            </tr>
+            <div id="pop4" class="popl"></div>
+<tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">116 canciones-48,777 fans</span></td>
+            </tr>
+          </div>
+          <div id="exitos"><a id="exitostxt">Lista de exitos ></a></div>
+          <div id="top_pais">
+            <div id="imgtop"></div>
                                             <table>
                                                 <tr>
                                                     <td>
@@ -93,26 +125,41 @@
                                                     </tr>
                                                 </tr>
                                             </table>
-    	  </div>
-    	  <div id="listacanciones">
-    	  	<h6>Canciones</h6>
-    	  	<div class="cancion"><span class="numc">01.</span><a class="nmbcancion">Iron Man</a><span class="artir">de </span><a class="arti">Black Sabbath</a></div>
-    	  	<div class="cancion"><span class="numc">02.</span><a class="nmbcancion">Land of confusion</a><span class="artir">de </span><a class="arti">Genesis</a></div>
-    	  	<div class="cancion"><span class="numc">03.</span><a class="nmbcancion">I want out</a><span class="artir">de </span><a class="arti">Helloween</a></div>
-    	  	<div class="cancion"><span class="numc">04.</span><a class="nmbcancion">Decadence</a><span class="artir">de </span><a class="arti">Disturbed</a></div>
-    	  	<div class="cancion"><span class="numc">05.</span><a class="nmbcancion">Evolution</a><span class="artir">de </span><a class="arti">Pearl Jam</a></div>
-    	  </div>
-    	  <div id="lanzamientos"><a id="lanzamientostxt">Nuevos Lanzamientos ></a></div>
-    	  <div id="lanzamiento">
-    	  	<div id="lanzamiento1" class="lanzamiet">
-   
-            </div>
-    	  	<div id="lanzamiento2" class="lanzamiet"></div>
-    	  	<div id="lanzamiento3" class="lanzamiet"></div>
-    	  	<div id="lanzamiento4" class="lanzamiet"></div>
-    	  </div>
+                <div class="btnreproducir"><span class="glyphicon glyphicon-play"></span><span id="btn">Reproducir</span></div>
+          </div>
+          <div id="listacanciones">
+            <h6>Canciones</h6>
+            <div class="cancion"><span class="numc">01.</span><a class="nmbcancion">Iron Man</a><span class="artir">de </span><a class="arti">Black Sabbath</a></div>
+            <div class="cancion"><span class="numc">02.</span><a class="nmbcancion">Land of confusion</a><span class="artir">de </span><a class="arti">Genesis</a></div>
+            <div class="cancion"><span class="numc">03.</span><a class="nmbcancion">I want out</a><span class="artir">de </span><a class="arti">Helloween</a></div>
+            <div class="cancion"><span class="numc">04.</span><a class="nmbcancion">Decadence</a><span class="artir">de </span><a class="arti">Disturbed</a></div>
+            <div class="cancion"><span class="numc">05.</span><a class="nmbcancion">Evolution</a><span class="artir">de </span><a class="arti">Pearl Jam</a></div>
+          </div>
+          <div id="lanzamientos"><a id="lanzamientostxt">Nuevos Lanzamientos ></a></div>
+          <div id="lanzamiento">
+            <div id="lanzamiento1" class="lanzamiet"></div>
+            <tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">De banda</span></td>
+            </tr>
+            <div id="lanzamiento2" class="lanzamiet"></div>
+            <tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">De banda</span></td>
+            </tr>
+            <div id="lanzamiento3" class="lanzamiet"></div>
+            <tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">De banda</span></td>
+            </tr>
+            <div id="lanzamiento4" class="lanzamiet"></div>
+            <tr>
+             <td><a class="nombre_album">Album recomendado</a></td>
+             <td><span class="cantidadfans">De banda</span></td>
+            </tr>
+          </div>
         </center>
-    </div>	
+    </div>  
 
   </center>
 
@@ -121,7 +168,7 @@
 
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/discografia.js"></script>
+<script type="text/javascript" src="js/inicio.js"></script>
 
 </body>
 </html>
